01f4d6f32008be29abe34ac433a18acbf1d739a8
andrewdavis23 <47924318+andrewdavis23@users.noreply.github.com>
2022-01-29 13:09:10 -0500

    Update hike_monitor.py
    final replit code

--- hike_monitor.py
+++ hike_monitor.py
@@ -1,83 +1,96 @@
 import smtplib
 import requests
 import bs4
+import os
+from keep_alive import keep_alive
+from time import sleep
+from random import randint
 
 titles = []
+titles_old = []
 links = []
 details = []
 message = ''
 any_new = False
 new_count = 0
-
-# user name and password
-with open('C:\Python Programs\Web Monitor\UNAP.txt', 'r') as file:
-    unap = file.read().split('\n')
+psw = os.environ['Hike.Monitor@gmail.com']
 
 # Gmail connection
 server = smtplib.SMTP_SSL( 'smtp.gmail.com', 465 )
-server.login( unap[0], unap[1] )
+server.login( 'Hike.Monitor@gmail.com', psw )
 
-# access previous webpage
-with open('C:\Python Programs\Web Monitor\OLDPAGE.txt', 'r') as file:
-    wp_old = file.read()
+# HTTP server
+keep_alive()
 
-# access current webpage
-wp_new = requests.get('https://activities.outdoors.org/search/').text
+while True:
 
-# pull out piece of HTML that contains data
-soup_old = bs4.BeautifulSoup(wp_old, 'html.parser')
-soup_activities_old = soup_old.find(id='actdb-results')
-soup_list_old = soup_activities_old.find_all(id='ListViewContent')
+  # access previous webpage
+  with open(r'OLDPAGE.txt', 'r') as file:
+      wp_old = file.read()
 
-# extract hikes from old web page
-for i in soup_list_old:
-    titles.append(i.a.get_text())
-    links.append(i.a.get('href')[2:])
-    details.append(i.find('div', class_='details').get_text())
+  # access current webpage
+  wp_new = requests.get('https://activities.outdoors.org/search/').text
 
-################################### test ######################################
-print('OLD HIKES\n',titles)
+  # pull out piece of HTML that contains data
+  soup_old = bs4.BeautifulSoup(wp_old, 'html.parser')
+  soup_activities_old = soup_old.find(id='actdb-results')
+  soup_list_old = soup_activities_old.find_all(id='ListViewContent')
 
-# pull out piece of HTML that contains data
-soup_new = bs4.BeautifulSoup(wp_new, 'html.parser')
-soup_activities_new = soup_new.find(id='actdb-results')
-soup_list_new = soup_activities_new.find_all(id='ListViewContent')
+  # extract hikes from old web page
+  for i in soup_list_old:
+      titles_old.append(i.a.get_text())
 
-# extract hikes from new web page and check for new hikes
-for i in soup_list_new:
-    title_i = i.a.get_text()
-    link_i = i.a.get('href')[2:]
-    details_i = i.find('div', class_='details').get_text()
+  # pull out piece of HTML that contains data
+  soup_new = bs4.BeautifulSoup(wp_new, 'html.parser')
+  soup_activities_new = soup_new.find(id='actdb-results')
+  soup_list_new = soup_activities_new.find_all(id='ListViewContent')
 
-    if title_i not in titles:
-        any_new = True
-        new_count += 1
-        chunk = title_i +'\n'+ details_i +'\n'+ link_i +'\n'*2
-        chunk = chunk.replace(':','.')
-        chunk = chunk.replace('Activity',' | Activity')
-        message += chunk
+  # extract hikes from new web page and check for new hikes
+  for i in soup_list_new:
+      title = i.a.get_text()
 
-################################### test ######################################
-print('any_new? ',str(any_new))
+      if title not in titles_old:
+          link = i.a.get('href')[2:]
+          details_temp = i.find('div', class_='details').get_text().replace('Activity',' | Activity')
+          dict_temp = {}
+          for j in details_temp.split('|'):
+              content = j.strip().partition(':')
+              dict_temp[content[0].strip()] = content[2].strip()
+          any_new = True
+          new_count += 1
+          titles.append(title)
+          links.append(link)
+          details.append(dict_temp)
+          chunk = title 
+          for i in dict_temp:
+              chunk += '\n {} {} '.format(i.upper(),dict_temp[i])
+          chunk += '\n'+ link +'\n'*2
+          chunk = chunk.replace(':','.')
+          message += chunk
 
-if any_new:
-    # # replace cached webpage
-    # with open('C:\Python Programs\Web Monitor\OLDPAGE.txt', 'w') as file:
-    #     file.write(wp_new)
+  if any_new:
+      print(message)
 
-    # subscriber list
-    with open('C:\Python Programs\Web Monitor\SUBS.txt', 'r') as file:
-        rec_emails = file.read().split('\n')
+      # replace cached webpage
+      with open(r'OLDPAGE.txt', 'w') as file:
+          file.write(wp_new)
 
-    # add to email: subject and footnote
-    plur = 's' if new_count > 1 else ''
-    footnote = 'If you have any questions, comments, need to unsubscribe, reply to this email.'
-    
-    message = 'Subject: {} New Hike{} Posted!\n\n{}\n\n{}'.format(new_count,plur,message,footnote)
+      # subscriber list
+      with open(r'SUBS.txt', 'r') as file:
+          rec_emails = file.read().split('\n')
 
-    ################################### test ######################################
-    print(message)
+      # add to email: subject and footnote
+      plur = 's' if new_count > 1 else ''
+      footnote = 'If you have any questions, comments, need to unsubscribe, reply to this email.'
+      
+      message = 'Subject: {} New Hike{} Posted!\n\n{}\n\n{}'.format(new_count,plur,message,footnote)
 
-    server.sendmail(from_addr='Hike.Monitor@gmail.com', to_addrs=rec_emails, msg=message)
-    server.quit()
+      server.sendmail(from_addr='Hike.Monitor@gmail.com', to_addrs=rec_emails, msg=message)
+      server.quit()
+  else:
+      print('NO NEW HIKES')
+
+  # ping at random intervals
+  wait = randint(5,10)
+  sleep(60*wait)
 
